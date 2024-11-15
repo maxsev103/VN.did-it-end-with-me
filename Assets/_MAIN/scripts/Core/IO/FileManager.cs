8699fbc6a894b732b63494362a426fdc072c6eba
Max <severo.max03@gmail.com>
2024-11-15 15:24:47 +0800

    File Reading tests

--- Assets/_MAIN/scripts/Core/IO/FileManager.cs
+++ Assets/_MAIN/scripts/Core/IO/FileManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class FileManager : MonoBehaviour
+public class FileManager
 {
-    // Start is called before the first frame update
-    void Start()
+    public static List<string> ReadTextFile(string filePath, bool includeBlankLines = true)
     {
-        
+        if (filePath.StartsWith('/'))
+            filePath = FilePaths.root + filePath;
+
+        List<string> lines = new List<string>();
+        try
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    if (includeBlankLines || !string.IsNullOrWhiteSpace(line))
+                        lines.Add(line);
+                }
+            }
+        } 
+        catch (FileNotFoundException ex)
+        {
+            Debug.LogError($"File not found: '{ex.FileName}'");
+        }
+
+        return lines;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static List<string> ReadTextAsset(string filePath, bool includeBlankLines = true)
     {
-        
+        return null;
     }
 }
