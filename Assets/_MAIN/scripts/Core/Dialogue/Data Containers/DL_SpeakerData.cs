bb4d9dbc49b84ef4b020ad4831b49966641057bf
Max <severo.max03@gmail.com>
2024-11-16 15:57:59 +0800

    Starting Speaker Data

---
+++ Assets/_MAIN/scripts/Core/Dialogue/Data Containers/DL_SpeakerData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DL_SpeakerData
+{
+    public string name, castName;
+    public Vector2 castPosition;
+}
